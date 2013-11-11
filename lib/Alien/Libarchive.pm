fd5ca38579d76251f7e74ee5e67c9b68dc22c8d2
Graham Ollis <perl@wdlabs.com>
2013-11-11 14:26:57 -0500

    macro extractor

--- lib/Alien/Libarchive.pm
+++ lib/Alien/Libarchive.pm
@@ -8,4 +8,38 @@ use base 'Alien::Base';
 # ABSTRACT: Build and make available libarchive
 # VERSION
 
+# extract the macros from the header files, this is a private function
+# because it may not be portable.  Used by Archive::Libarchive::XS
+# (and maybe Archive::Libarchive::FFI) to automatically generate
+# constants
+sub _macro_list
+{
+  require Config;
+  require File::Temp;
+  require File::Spec;
+
+  my $alien = Alien::Libarchive->new;  
+  my $cc = "$Config::Config{ccname} $Config::Config{ccflags} " . $alien->cflags;
+  
+  my $fn = File::Spec->catfile(File::Temp::tempdir( CLEANUP => 1 ), "test.c");
+  
+  do {
+    open my $fh, '>', $fn;
+    print $fh "#include <archive.h>\n";
+    print $fh "#include <archive_entry.h>\n";
+    close $fh;
+  };
+  
+  my @list;
+  my $cmd = "$cc -E -dM $fn";
+  foreach my $line (`$cmd`)
+  {
+    if($line =~ /^#define ((AE|ARCHIVE)_\S+)/)
+    {
+      push @list, $1;
+    }
+  }
+  sort @list;
+}
+
 1;
