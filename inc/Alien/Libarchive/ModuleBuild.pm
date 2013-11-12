42c84efdbe37f8846e96bb5a6c2c24c3a0d8a7a6
Graham Ollis <perl@wdlabs.com>
2013-11-12 10:55:51 -0500

    add prereqs Alien::OpenSSL, Alien::LibXML

---
+++ inc/Alien/Libarchive/ModuleBuild.pm
@@ -0,0 +1,27 @@
+package Alien::Libarchive::ModuleBuild;
+
+use strict;
+use warnings;
+use Alien::LibXML;
+use Alien::OpenSSL;
+use base qw( Alien::Base::ModuleBuild );
+
+sub alien_do_commands
+{
+  my($self, $phase) = @_;
+
+  local $ENV{CFLAGS} = $ENV{CFLAGS};
+  local $ENV{LIBS}   = $ENV{LIBS};
+  foreach my $alien (map { $_->new } qw( Alien::LibXML Alien::OpenSSL ))
+  {
+    $ENV{CFLAGS} .= ' ' . $alien->cflags;
+    $ENV{LIBS}   .= ' ' . $alien->libs;
+  }
+  
+  #print "\n\nCFLAGS = $ENV{CFLAGS}\n";
+  #print "LIBS   = $ENV{LIBS}\n\n\n";
+  
+  $self->SUPER::alien_do_commands($phase);
+}
+
+1;
