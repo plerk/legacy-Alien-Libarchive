8576647c6086bfdd7feeb3ccdee28fefdd478ec0
Graham Ollis <perl@wdlabs.com>
2013-11-14 16:49:16 -0500

    documentation

--- README.md
+++ README.md
@@ -2,6 +2,45 @@
 
 Build and make available libarchive
 
+# SYNOPSIS
+
+Build.PL
+
+    use Alien::Libarchive;
+    use Module::Build;
+    
+    my $alien = Alien::Libarchive->new;
+    my $build = Module::Build->new(
+      ...
+      extra_compiler_flags => $alien->cflags,
+      extra_linker_flags   => $alien->libs,
+      ...
+    );
+    
+    $build->create_build_script
+
+# DESCRIPTION
+
+This distribution installs libarchive so that it can be used by other
+Perl distributions.  If it can find the development package for libarchive
+for your operating system it will use that, otherwise it will download
+libarchive, build and install that.
+
+# METHODS
+
+## cflags
+
+Returns the C compiler flags necessary to build against libarchive.
+
+## libs
+
+Returns the library flags necessary to build against libarchive.
+
+# SEE ALSO
+
+- [Archive::Libarchive::XS](https://metacpan.org/pod/Archive::Libarchive::XS)
+- [Archive::Libarchive::FFI](https://metacpan.org/pod/Archive::Libarchive::FFI)
+
 # AUTHOR
 
 Graham Ollis <plicease@cpan.org>
--- lib/Alien/Libarchive.pm
+++ lib/Alien/Libarchive.pm
@@ -8,10 +8,57 @@ use base 'Alien::Base';
 # ABSTRACT: Build and make available libarchive
 # VERSION
 
+=head1 SYNOPSIS
+
+Build.PL
+
+ use Alien::Libarchive;
+ use Module::Build;
+ 
+ my $alien = Alien::Libarchive->new;
+ my $build = Module::Build->new(
+   ...
+   extra_compiler_flags => $alien->cflags,
+   extra_linker_flags   => $alien->libs,
+   ...
+ );
+ 
+ $build->create_build_script
+
+=head1 DESCRIPTION
+
+This distribution installs libarchive so that it can be used by other
+Perl distributions.  If it can find the development package for libarchive
+for your operating system it will use that, otherwise it will download
+libarchive, build and install that.
+
+=head1 METHODS
+
+=head2 cflags
+
+Returns the C compiler flags necessary to build against libarchive.
+
+=head2 libs
+
+Returns the library flags necessary to build against libarchive.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Archive::Libarchive::XS>
+
+=item L<Archive::Libarchive::FFI>
+
+=back
+
+=cut
+
 # extract the macros from the header files, this is a private function
 # because it may not be portable.  Used by Archive::Libarchive::XS
 # (and maybe Archive::Libarchive::FFI) to automatically generate
 # constants
+# UPDATE: this maybe should use C::Scan or C::Scan::Constants
 sub _macro_list
 {
   require Config;
