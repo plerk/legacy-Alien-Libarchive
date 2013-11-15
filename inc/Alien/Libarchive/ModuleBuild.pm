6b8811a119bd14620a11a248a36de33aa6332ebe
Graham Ollis <perl@wdlabs.com>
2013-11-15 10:13:52 -0500

    for now don't use Alien::{LibXML,OpenSSL} at least until I can better understand the benefits / consequences

--- inc/Alien/Libarchive/ModuleBuild.pm
+++ inc/Alien/Libarchive/ModuleBuild.pm
@@ -2,24 +2,24 @@ package Alien::Libarchive::ModuleBuild;
 
 use strict;
 use warnings;
-use Alien::LibXML;
-use Alien::OpenSSL;
+## use Alien::LibXML;
+## use Alien::OpenSSL;
 use base qw( Alien::Base::ModuleBuild );
 
 sub alien_do_commands
 {
   my($self, $phase) = @_;
 
-  local $ENV{CFLAGS} = $ENV{CFLAGS};
-  local $ENV{LIBS}   = $ENV{LIBS};
-  foreach my $alien (map { $_->new } qw( Alien::LibXML Alien::OpenSSL ))
-  {
-    $ENV{CFLAGS} .= ' ' . $alien->cflags;
-    $ENV{LIBS}   .= ' ' . $alien->libs;
-  }
-  
-  #print "\n\nCFLAGS = $ENV{CFLAGS}\n";
-  #print "LIBS   = $ENV{LIBS}\n\n\n";
+##   local $ENV{CFLAGS} = $ENV{CFLAGS};
+##   local $ENV{LIBS}   = $ENV{LIBS};
+##   foreach my $alien (map { $_->new } qw( Alien::LibXML Alien::OpenSSL ))
+##   {
+##     $ENV{CFLAGS} .= ' ' . $alien->cflags;
+##     $ENV{LIBS}   .= ' ' . $alien->libs;
+##   }
+##   
+##   #print "\n\nCFLAGS = $ENV{CFLAGS}\n";
+##   #print "LIBS   = $ENV{LIBS}\n\n\n";
   
   $self->SUPER::alien_do_commands($phase);
 }
